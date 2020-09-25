09c698ee8f44c61c095b4dd73bf40c02b386443e
Lars <lrecht16@student.aau.dk>
2020-09-25 14:39:11 +0200

    added backtrackmap

--- benchmarks/dijkstra/functional_py.py
+++ benchmarks/dijkstra/functional_py.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from functools import reduce
+from pyrsistent import pmap
 
 edges = [("a", "b", 7),  ("a", "c", 9),
          ("a", "f", 14), ("b", "c", 10),
@@ -7,27 +8,38 @@ edges = [("a", "b", 7),  ("a", "c", 9),
          ("c", "f", 2),  ("d", "e", 6),
          ("e", "f", 9)]
 
-def getNewMoves(edges, oldPositions, pos, cost):
-    moves = list(filter(lambda move: move[0] == pos, edges))
-    movesToNewNodes = list(filter(lambda move: filter(lambda oldPos: move[1] == oldPos[0], oldPositions),moves))
 
+def getNewMoves(edges, backtrackMap, pos, cost):
+    # Filters all edges that are either the current pos or have already been visited
+    moves = list(filter(lambda move: move[0] == pos and 
+                                     move[1] not in backtrackMap.keys(), 
+                                     edges))
+
+    # Adds the cost of taking an edge
     return list(map(lambda move: (move[0], move[1], move[2] + cost), moves))
 
-def move(legalMoves, paths, dest):
+def backtrack(backtrackMap, position, result):
+    if position == '0':
+        return result
+    else:
+        return backtrack(backtrackMap, backtrackMap[position], [position] + result)
+
+def move(legalMoves, backtrackMap, dest):
     nextMove = reduce(lambda minimum, newMove:
                 minimum if minimum[1] == dest 
                 else (newMove if newMove[2] < minimum[2] or newMove[1] == dest else minimum),legalMoves)
 
     if nextMove[1] == dest:
-        return list(filter(lambda path: path[0] == nextMove[0],paths))[0][1] + [(nextMove[0],nextMove[1])]
+        return backtrack(backtrackMap,nextMove[0],[nextMove[1]])
     else:
-        newPaths = paths + [(nextMove[1], list(filter(lambda path: path[0] == nextMove[0],paths))[0][1] + [(nextMove[0],nextMove[1])])]
-        oldMoves = list(filter(lambda a: a != nextMove,legalMoves))
-        newMoves = getNewMoves(edges,newPaths,nextMove[1],nextMove[2]) + oldMoves
-        return move(newMoves, newPaths, dest)
+        newBacktrackMap = backtrackMap.update({nextMove[1]:nextMove[0]})
+        oldMoves = list(filter(lambda a: a[1] != nextMove[1],legalMoves))
+        newMoves = getNewMoves(edges,newBacktrackMap,nextMove[1],nextMove[2]) + oldMoves
+        return move(newMoves, newBacktrackMap, dest)
+
 
 def shortestPath(edges, start, dest):
-    moves = getNewMoves(edges, [(start,[])], start, 0)
-    return move(moves, [(start,[])], dest)
+    moves = getNewMoves(edges, pmap({'a':'0'}), start, 0)
+    return move(moves, pmap({'a':'0'}), dest)
 
 shortestPath(edges, "a", "e")
