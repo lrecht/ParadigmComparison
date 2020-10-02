d260bac96c1cfdcd816e421403b379d4f9542826
Caspersn2 <caspersn2@gmail.com>
2020-10-02 11:28:21 +0200

    I think it works

--- benchmarks/dijkstra/procedual_f#/Program.fs
+++ benchmarks/dijkstra/procedual_f#/Program.fs
@@ -3,127 +3,106 @@
 open System
 open System.Collections.Generic
 
-type Vertex = 
-    {
-        Key: char
-        Dist: int32
-    }
+let initVertices =
+    let vertices = new Dictionary<char, int32>()
+    vertices.Add('a', Int32.MaxValue)
+    vertices.Add('b', Int32.MaxValue)
+    vertices.Add('c', Int32.MaxValue)
+    vertices.Add('d', Int32.MaxValue)
+    vertices.Add('e', Int32.MaxValue)
+    vertices.Add('f', Int32.MaxValue)
+    vertices
 
-type Edge = 
-    { 
-        Start: Vertex
-        End: Vertex
-        Cost: int
-    }
+let initPrevDic =
+    let prevDic = new Dictionary<char, Nullable<char>>()
+    prevDic.Add('a', Nullable())
+    prevDic.Add('b', Nullable())
+    prevDic.Add('c', Nullable())
+    prevDic.Add('d', Nullable())
+    prevDic.Add('e', Nullable())
+    prevDic.Add('f', Nullable())
+    prevDic
 
+let initEdges =
+    let edges = new Dictionary<char, (char*int) array>()
+    edges.Add('a', [|('b', 7);('c', 9);('f', 14)|])
+    edges.Add('b', [|('c', 10);('d', 15)|])
+    edges.Add('c', [|('d', 11);('f', 2)|])
+    edges.Add('d', [|('e', 6)|])
+    edges.Add('e', [|('f', 9)|])
+    edges
 
-let setVertexDist (array: Vertex array) (key: char) (value: int) =
-    let rec loop i = 
-        if i >= array.Length then
-            raise (System.Exception("The given element could not be found in the array"))
-        else if array.[i].Key = key then
-            Array.set array i { Vertex.Key=key; Vertex.Dist=value }
-            array
-        else
-            loop (i+1)
-    loop 0
+let findMin (vertices: Dictionary<char, int32>) =
+    let mutable min = Int32.MaxValue
+    let mutable minElm = 'x'
+    for vertex in vertices do
+        if (vertex.Value < min) then
+            min <- vertex.Value
+            minElm <- vertex.Key
+    minElm
 
-let remove (array: Vertex array) index =
-    let rec loop i newArray =
-        if i >= array.Length then 
+let remove (array: char array) (elm: char) =
+    let rec loop i (newArray: char array) =
+        if i >= (array.Length) then
             newArray
-        else if i = index then
-            loop (i+1) newArray
-        else
-            let extra = [| array.[i] |]
-            loop (i+1) (Array.append newArray extra)
-
-    loop 0 [| |]
-
-let findNeighbors (array: Edge array) (elm: Vertex) =
-    let rec loop i neighbors = 
-        if (i >= array.Length) then
-            neighbors
-        else if (array.[i]).Start.Key = elm.Key then
-            let extra = [| array.[i] |]
-            loop (i+1) (Array.append neighbors extra)
-        else
-            loop (i+1) neighbors
-
+        else if array.[i] = elm then
+            let rest = array.[(i+1) .. (array.Length-1)]
+            Array.append newArray rest
+        else 
+            loop (i+1) (Array.append newArray [| array.[i] |])
     loop 0 [|  |]
 
-let includes (array: Vertex array) (key: char) =
-    let rec loop i = 
-        if array.Length <= i then
-            false
-        else if array.[i].Key = key then
-            true
-        else loop (i+1)
-    loop 0
+let insertSort (array: char array) (costMap: Dictionary<char, int32>) (newValue: char) =
+    let rec loop i newArray =
+        if i >= array.Length then
+            Array.append newArray [| newValue |]
+        else if costMap.[newValue] < costMap.[array.[i]] then
+            let rest = Array.append [| newValue |] array.[i .. (array.Length-1)]
+            Array.append newArray rest
+        else 
+            loop (i+1) (Array.append newArray [| array.[i] |])
+    loop 0 [|  |]
 
 [<EntryPoint>]
 let main argv =
-    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
-    let mutable vertices: Vertex array = [| 
-        {Vertex.Key='a'; Vertex.Dist=Int32.MaxValue }
-        {Vertex.Key='b'; Vertex.Dist=Int32.MaxValue }
-        {Vertex.Key='c'; Vertex.Dist=Int32.MaxValue }
-        {Vertex.Key='d'; Vertex.Dist=Int32.MaxValue }
-        {Vertex.Key='e'; Vertex.Dist=Int32.MaxValue }
-        {Vertex.Key='f'; Vertex.Dist=Int32.MaxValue }
-        |]
-    let edges = [|
-        { Edge.Start = vertices.[0]; End = vertices.[1]; Cost = 7 }
-        { Edge.Start = vertices.[0]; End = vertices.[2]; Cost = 9 }
-        { Edge.Start = vertices.[0]; End = vertices.[5]; Cost = 14 }
-        { Edge.Start = vertices.[1]; End = vertices.[2]; Cost = 10 }
-        { Edge.Start = vertices.[1]; End = vertices.[3]; Cost = 15 }
-        { Edge.Start = vertices.[2]; End = vertices.[3]; Cost = 11 }
-        { Edge.Start = vertices.[2]; End = vertices.[5]; Cost = 2 }
-        { Edge.Start = vertices.[3]; End = vertices.[4]; Cost = 6 }
-        { Edge.Start = vertices.[4]; End = vertices.[5]; Cost = 9 }
-        |]
-    
-    let mutable prevDic = new Dictionary<char, Char>()
+    let mutable vertices = initVertices
+    let mutable edges = initEdges
+    let mutable prevDic = initPrevDic
+
     let source = 'a'
-    let destination = 'e'
+    let mutable destination: Nullable<char> = Nullable('e')
 
-    //Set start vertex dist to 0
-    vertices <- setVertexDist vertices source 0
-    let mutable Q = [| vertices.[0] |]
+    vertices.[source] <- 0
+    
+    let mutable vertex_queue: char array = [| source |]
+    let mutable current: char = 'x'
 
-    while Q.Length > 0 do
-        let u = Q.[0]
-        Q <- remove Q 0
-        
-        //Foreach neighbor
-        let neighbors = findNeighbors edges u
-        for neighbor in neighbors do
-            let alt = u.Dist + neighbor.Cost
-            if alt < neighbor.End.Dist then
-                vertices <- setVertexDist vertices neighbor.End.Key alt
-                if not (includes Q neighbor.End.Key ) then
-                    Q <- Array.append Q [| neighbor.End |]
-                
-                if prevDic.ContainsKey(neighbor.End.Key) then
-                    prevDic.Item neighbor.End.Key <- u.Key
-                else 
-                    prevDic.Add(neighbor.End.Key, u.Key)
+    while vertex_queue.Length > 0 do 
+        current <- vertex_queue.[0] //findMin vertices
+        vertex_queue <- remove vertex_queue current
+        let mutable fail = false
+        if not (edges.ContainsKey current) then
+            fail <- true
+        if not fail then
+            let neighbors = edges.[current]
+            for (key, value) in neighbors do
+                let alt: int32 = vertices.[current] + value
+                if alt < vertices.[key] then
+                    vertex_queue <- remove vertex_queue key
+                    vertices.[key] <- alt
+                    prevDic.[key] <- Nullable(current)
+                    vertex_queue <- insertSort vertex_queue vertices key
+    
+    //Create the shortest path
+    let mutable shortestPath: (char array) = [| |]
+    let mutable previous = destination
+    while previous <> Nullable() do
+        shortestPath <- Array.append [| previous.Value |] shortestPath
+        previous <- prevDic.[previous.Value]
 
-    let mutable s: char array = [| |]
-    let mutable u = destination
-    while u <> source && u <> 'z' do
-        s <- Array.append s [| u |]
-        u <- prevDic.GetValueOrDefault u
-    s <- Array.append s [| u |]
-
-    printfn "Route:"
-    for place in s do
-        printf "%c" place
+    for part in shortestPath do
+        printf "%c " part
     printfn ""
-
-    stopWatch.Stop()
-    printfn "Milliseconds: %f" stopWatch.Elapsed.TotalMilliseconds
 
     0 // return an integer exit code
 
