05dca62fdba3d3db81b65bf2af48b061995335bd
Lars <lrecht16@student.aau.dk>
2020-10-02 10:48:35 +0200

    Added a translation from the list of edges to a map, to avoid searching through the list of edges too many times

--- benchmarks/dijkstra/functional_f#/Program.fs
+++ benchmarks/dijkstra/functional_f#/Program.fs
@@ -2,10 +2,9 @@
 
 open System
 
-
-let graph = [("a", "b", 7);  ("a", "c", 9);  ("a", "f", 14); ("b", "c", 10);
-               ("b", "d", 15); ("c", "d", 11); ("c", "f", 2);  ("d", "e", 6);
-               ("e", "f", 9)]
+let graph = [('a', 'b', 7);  ('a', 'c', 9);  ('a', 'f', 14); ('b', 'c', 10);
+               ('b', 'd', 15); ('c', 'd', 11); ('c', 'f', 2);  ('d', 'e', 6);
+               ('e', 'f', 9)]
 
 let rec merge xs ys res = 
   match xs,ys with
@@ -14,29 +13,37 @@ let rec merge xs ys res =
      if x < y then merge xs' ys (List.append res [(a,b,x)])
      else merge xs ys' (List.append res [(a',b',y)])
 
-let getNewMoves graph (visited:Map<string,string>) position cost =
-    List.map(fun (a,b,c: int) -> (a,b,c+cost)) 
+let getNewMoves (edgeMap:Map<char,(char*char*int) list>) (visited:Map<char,char>) position cost =
+    List.map (fun (a,b,c: int) -> (a,b,c+cost)) 
         (List.filter (fun (a,b,c) -> not (visited.ContainsKey b)) 
-            (List.filter (fun (a,b,c) -> a = position) graph))
+            (if edgeMap.ContainsKey position then edgeMap.[position] else []))
 
-let rec backtrack (visited:Map<string,string>) curr res start =
+let rec backtrack (visited:Map<char,char>) curr res start =
     if curr = start then (curr::res)
     else backtrack visited (visited.[curr]) (curr::res) start
 
-let rec findPath graph (moves:(string * string * int) list) (visited:Map<string,string>) dest =
+let rec findPath edgeMap (moves:(char * char * int) list) (visited:Map<char,char>) dest =
     let from,target,cost = moves.Head in
         if target = dest then
             visited.Add(target,from)
             else
-            findPath graph (merge (moves.Tail) (getNewMoves graph visited target cost) []) (visited.Add(target,from)) dest
+            findPath edgeMap (merge (moves.Tail) 
+                                    (getNewMoves edgeMap visited target cost)
+                                    []) 
+                             (visited.Add(target,from)) 
+                             dest
 
-
-let dijkstraPath graph start dest = 
+let dijkstraPath edgeMap start dest = 
     if start = dest then [start] else
-        backtrack (findPath graph (getNewMoves graph Map.empty start 0) Map.empty dest) dest [] start
-
+        backtrack (findPath edgeMap 
+                            (getNewMoves edgeMap Map.empty start 0) 
+                            Map.empty dest)
+                  dest [] start
 
 [<EntryPoint>]
 let main argv =
-    printfn "%A" (dijkstraPath (List.sortBy (fun (a,b,c) -> c) graph) "a" "e")
+    let edgeMap = List.fold (fun (acc:Map<char,(char*char*int) list>) (from,dest,cost) -> 
+        if acc.ContainsKey from then acc.Add(from,((from,dest,cost)::(acc.[from])))
+        else acc.Add(from,[from,dest,cost])) Map.empty graph in
+    printfn "%A" (dijkstraPath edgeMap 'a' 'e')
     0 // return an integer exit code
