65dc0d2cc9f5f1e85511c6fd0b275fd74d72172f
Caspersn2 <caspersn2@gmail.com>
2020-10-02 14:11:59 +0200

    Now strings, and read from file

--- benchmarks/dijkstra/procedual_f#/Program.fs
+++ benchmarks/dijkstra/procedual_f#/Program.fs
@@ -1,31 +1,46 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.IO
 open System.Collections.Generic
 
-let initVertices (edges: Dictionary<char, (char*int) array>) =
-    let vertices = new Dictionary<char, int32>()
+let initVertices (edges: Dictionary<string, (string*int) array>) =
+    let vertices = new Dictionary<string, int32>()
     for edge in edges.Keys do
         vertices.Add(edge, Int32.MaxValue)
     vertices
 
-let initPrevDic (edges: Dictionary<char, (char*int) array>) =
-    let prevDic = new Dictionary<char, Nullable<char>>()
+let initPrevDic (edges: Dictionary<string, (string*int) array>) =
+    let prevDic = new Dictionary<string, string>()
     for edge in edges.Keys do
-        prevDic.Add(edge, Nullable())
+        prevDic.Add(edge, null)
     prevDic
 
-let initEdges =
-    let edges = new Dictionary<char, (char*int) array>()
-    edges.Add('a', [|('b', 7);('c', 9);('f', 14)|])
-    edges.Add('b', [|('c', 10);('d', 15)|])
-    edges.Add('c', [|('d', 11);('f', 2)|])
-    edges.Add('d', [|('e', 6)|])
-    edges.Add('e', [|('f', 9)|])
-    edges.Add('f', [| |])
-    edges
+let toDictionary (map : Map<_, _>) : Dictionary<_, _> = Dictionary(map)
 
-let remove (array: char array) (elm: char) =
+let initEdges =
+    let filePath = "../graph.csv"
+    let lines = seq {
+        use sr = new StreamReader (filePath)
+        while not sr.EndOfStream do
+            yield sr.ReadLine ()
+    }
+
+    let graph = 
+        Seq.map 
+            (fun (a:string) -> 
+                let words = (a.Split ',') 
+                in (words.[0],words.[1],(words.[2] |> int)))
+            lines
+    
+    let edgeMap = Seq.fold (fun (acc:Map<string,(string*int) array>) (from,dest,cost) -> 
+        if acc.ContainsKey from then 
+            acc.Add(from,(Array.append (acc.[from]) [| (dest,cost)|] ))
+        else acc.Add(from,[|dest,cost|])) Map.empty graph in
+
+    toDictionary edgeMap
+
+let remove (array: string array) (elm: string) =
     let rec loop i =
         if i >= (array.Length) then
             i
@@ -36,10 +51,11 @@ let remove (array: char array) (elm: char) =
     let index = loop 0
     Array.append array.[0 .. (index-1)] array.[(index+1) .. (array.Length-1)]
 
-let insertSort (array: char array) (costMap: Dictionary<char, int32>) (newValue: char) =
+let insertSort (array: string array) (costMap: Dictionary<string, int32>) (newValue: string) =
     let rec loop i =
         if i >= array.Length then
             i
+        //Try to change this
         else if costMap.[newValue] < costMap.[array.[i]] then
             i
         else 
@@ -60,41 +76,49 @@ let main argv =
     let mutable vertices = initVertices edges
     let mutable prevDic = initPrevDic edges
 
-    let source = 'a'
-    let mutable destination: Nullable<char> = Nullable('e')
+    let source = "257"
+    let mutable destination: string = "5525"
 
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
     vertices.[source] <- 0
     
-    let mutable vertex_queue: char array = [| source |]
+    let mutable vertex_queue: string array = [| source |]
     
     while vertex_queue.Length > 0 do 
         let current = vertex_queue.[0]
         vertex_queue <- vertex_queue.[1 .. (vertex_queue.Length-1)]
-        let mutable continueNow = false
         
         //Destination found
-        if current = destination.Value then
+        if current = destination then
             vertex_queue <- [|  |]
-        else
+        else if vertices.ContainsKey(current) then
             let neighbors = edges.[current]
-            for (key, value) in neighbors do
-                let alt: int32 = vertices.[current] + value
-                if alt < vertices.[key] then
-                    vertex_queue <- remove vertex_queue key
-                    vertices.[key] <- alt
-                    prevDic.[key] <- Nullable(current)
-                    vertex_queue <- insertSort vertex_queue vertices key
+            
+            for (key, value) in neighbors do   
+                //If the neighbor does not have another neighbor, then it is not added to the vertices, and cannot work
+                //This fix however, will do so that we cannot find distinations if it has no edges going out...
+                if vertices.ContainsKey(key) then
+                    let alt: int32 = vertices.[current] + value
+                    if alt < vertices.[key] then
+                        vertex_queue <- remove vertex_queue key
+                        vertices.[key] <- alt
+                        prevDic.[key] <- current
+                        vertex_queue <- insertSort vertex_queue vertices key
     
     //Create the shortest path
-    let mutable shortestPath: (char array) = [| |]
+    let mutable shortestPath: (string array) = [| |]
     let mutable previous = destination
-    while previous <> Nullable() do
-        shortestPath <- Array.append [| previous.Value |] shortestPath
-        previous <- prevDic.[previous.Value]
+    while not (isNull previous) do
+        shortestPath <- Array.append [| previous |] shortestPath
+        previous <- prevDic.[previous]
 
+    stopWatch.Stop()
     for part in shortestPath do
-        printf "%c " part
+        printf "%s " part
     printfn ""
+    printfn "Steps: %i" shortestPath.Length
+    printfn "End weigth: %i" vertices.[destination]
+    printfn "Milliseconds: %i" stopWatch.ElapsedMilliseconds
 
     0 // return an integer exit code
 
