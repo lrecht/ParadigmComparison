b97a7893788fb9b0aaa72a69a1e2f4c636d31b9a
Lars <lrecht16@student.aau.dk>
2020-10-12 15:19:15 +0200

    Playing cards implemented functionally in f#, though still with a somewhat slow execurtion time

---
+++ benchmarks/playing_cards/functional_f#/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+type Value = Two=0 | Three=1 | Four=2 | Five=3 | Six=4 | Seven=5 | Eight=6 | Nine=7 | Ten=8 | Jack=9 | Queen=10 | King=11 | Ace=12
+type Suit = Diamonds=0 | Spades=1 | Hearts=2 | Clubs=3
+
+let newDeck = seq {for x in Enum.GetValues(typeof<Value>) do for y in Enum.GetValues(typeof<Suit>) -> x,y }
+
+let rand = Random()
+let shuffle xs = Seq.sortBy (fun x -> rand.Next()) xs
+
+let showDeck deck = 
+    String.concat "\n" (Seq.map (fun (v,s) -> sprintf "%O of %O" v s) deck)
+
+let deal deck =
+    (Seq.head deck, Seq.tail deck)
+
+let rec run' deck runs c count =
+    if runs = 0
+        then c
+    elif count = 0
+        then run' newDeck (runs-1) c 52
+    else
+        run' ((snd (deal (shuffle deck)))) runs ((String.length (showDeck deck)) + c) (count-1)
+let run runs =
+    run' newDeck runs 0 52
+
+[<EntryPoint>]
+let main argv =
+    printfn "%O" (run 1000)
+    0 // return an integer exit code
