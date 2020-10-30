bb9dc0a194bda143b1f423cc57119d5ab046078c
Caspersn2 <caspersn2@gmail.com>
2020-10-30 08:48:35 +0100

    playfair cipher in c# procedural

---
+++ benchmarks/playfair_cipher/procedural_c#/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Text;
+
+namespace procedural_c_
+{
+	class Program
+	{
+		const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+		const int dimension1 = 5;
+		const int dimension2 = 5;
+		static char[,] table;
+		static (int, int)[] positions;
+
+		static void Main(string[] args)
+		{
+			var stop = System.Diagnostics.Stopwatch.StartNew();
+			positions = new (int, int)[26];
+
+			table = new char[5, 5];
+			var text = System.IO.File.ReadAllText("../lines.txt");
+			var keyword = "This is a great keyword";
+
+			populateTable(preprocessText(keyword + alphabet));
+			text = preprocessText(text);
+			string encryption = encrypt(text);
+			string decryption = decrypt(encryption);
+
+			Console.WriteLine(encryption.Length + "\t");
+			Console.WriteLine(decryption.Length + "\t");
+			Console.WriteLine("Time: " + stop.ElapsedMilliseconds);
+		}
+
+		static string preprocessText(string text)
+		{
+			text = text.ToUpper().Replace("J", "I");
+			return Regex.Replace(text, "[^A-Z]", "");
+		}
+
+		static void populateTable(string text)
+		{
+			var textLength = text.Length;
+			var place = 0;
+			for (int i = 0; i < textLength; i++)
+			{
+				var pos = (place / dimension1, place % dimension1);
+				var character = text[i];
+				var charPos = (character - 'A');
+				if (!(positions[charPos] != (0, 0) || table[0, 0] == character))
+				{
+					table[pos.Item1, pos.Item2] = character;
+					positions[charPos] = pos;
+					place++;
+				}
+			}
+		}
+
+		static string encrypt(string text)
+		{
+			StringBuilder sb = new StringBuilder(text);
+			for (int i = 0; i < sb.Length; i += 2)
+			{
+				if (i == sb.Length - 1)
+				{
+					sb = sb.Append(sb.Length % 2 == 1 ? "X" : "");
+				}
+
+				else if (sb[i] == sb[i + 1])
+					sb = sb.Insert(i + 1, 'X');
+			}
+
+			return iterateOnPairs(sb.ToString(), 1);
+		}
+
+		static string decrypt(string text)
+		{
+			return iterateOnPairs(text, 4);
+		}
+
+		static string iterateOnPairs(string text, int direction)
+		{
+			if (text.Length % 2 == 1)
+				text = text + 'X';
+
+			var result = new StringBuilder();
+			int i = 0;
+			while (i < text.Length)
+			{
+				var first = positions[(text[i] - 'A')];
+				var second = positions[(text[i + 1] - 'A')];
+				result = result.Append(deencrypt(first, second, direction));
+				i += 2;
+			}
+			return result.ToString();
+		}
+
+		static string deencrypt((int, int) item1, (int, int) item2, int direction)
+		{
+			if (item1.Item2 == item2.Item2)
+			{
+				item1.Item1 = (item1.Item1 + direction) % 5;
+				item2.Item1 = (item2.Item1 + direction) % 5;
+			}
+			else if (item1.Item1 == item2.Item1)
+			{
+				item1.Item2 = (item1.Item2 + direction) % 5;
+				item2.Item2 = (item2.Item2 + direction) % 5;
+			}
+			else
+			{
+				int tmp = item1.Item1;
+				item1.Item1 = item2.Item1;
+				item2.Item1 = tmp;
+			}
+
+			char value1 = table[item1.Item1, item1.Item2];
+			char value2 = table[item2.Item1, item2.Item2];
+			return value1.ToString() + value2.ToString();
+		}
+	}
+}
