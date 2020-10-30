4d0f9a50741048684416f9ca0311a129d394b0ec
Caspersn2 <caspersn2@gmail.com>
2020-10-30 10:26:46 +0100

    Made it like in c#

--- benchmarks/playfair_cipher/procedural_f#/Program.fs
+++ benchmarks/playfair_cipher/procedural_f#/Program.fs
@@ -2,158 +2,95 @@
 
 open System
 open System.Text
+open System.Text.RegularExpressions
 
-let alphabet: string = "abcdefghijklmnopqrstuvwxyz"
-let mutable dimension1 = 0
-let mutable dimension2 = 0
+let alphabet: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let dimension1: int = 5
+let dimension2: int = 5
+let table: char[,] = Array2D.zeroCreate dimension1 dimension2
+let positions: (int*int)[] = Array.create 26 (0, 0)
 
-let substitute (value: char) =
-    if value = 'j' then
-        'i'
-    else
-        value
+let charValue (value: char) =
+    ((int value) - (int 'A'))
 
-let applySubstitutions (text: string) = 
-    let mutable result = ""
-    for i in 0 .. (text.Length-1) do
-        result <- result + (substitute text.[i]).ToString()
-    result
-
-let existsInTable (table: char[,]) (value: char) =
-    let mutable result = false
-    for i in 0 .. (dimension1-1) do
-        for j in 0 .. (dimension2-1) do
-            if table.[i, j] = value then
-                result <- true
-    result
-
-let populateKeyTable (table: char[,]) (keyword: string) = 
-    //Apply substitutions
-    let newKeyWord = applySubstitutions (keyword + alphabet)
-    
-    //Add the keyword to the table
-    let mutable pos = 0
-    for key in newKeyWord do
-        if not (existsInTable table key) then
-            table.[(pos/dimension1), (pos % dimension1)] <- Char.ToLower key
-            pos <- pos + 1
-
-let getCoordinates (value: char) (table: char[,]) =
-    let mutable result: (int * int) = (Int32.MaxValue, Int32.MaxValue)
-    for i in 0 .. (dimension1-1) do
-        for j in 0 .. (dimension2-1) do
-            if table.[i, j] = value then
-                result <- (i, j)
-    result
-
-let addOne (value: int) (max: int) =
-    if (value+1 >= max) then
-        0
-    else
-        value+1
-
-let subtractOne (value: int) (max: int) =
-    if (value-1 < 0) then
-        max-1
-    else
-        value-1
-
-let encryptPair (input1: char) (input2: char) (table: char[,]) =
-    let mutable (col1, row1) = getCoordinates input1 table
-    let mutable (col2, row2) = getCoordinates input2 table
-    if (row1 = row2) then
-        col1 <- addOne col1 dimension1
-        col2 <- addOne col2 dimension1
-    else if (col1 = col2) then
-        row1 <- addOne row1 dimension2
-        row2 <- addOne row2 dimension2
-    else
-        let temp = row1
-        row1 <- row2
-        row2 <- temp
-
-    (table.[col1, row1], table.[col2, row2])
-
-let decryptPair (input1: char) (input2: char) (table: char[,]) =
-    let mutable (col1, row1) = getCoordinates input1 table
-    let mutable (col2, row2) = getCoordinates input2 table
-    //The four rules:
-    if (row1 <> row2 && col1 <> col2) then
-        let temp = row1
-        row1 <- row2
-        row2 <- temp
-    else if (col1 = col2) then
-        row1 <- subtractOne row1 dimension2
-        row2 <- subtractOne row2 dimension2
-    else if (row1 = row2) then
-        col1 <- subtractOne col1 dimension1
-        col2 <- subtractOne col2 dimension1
-    
-    (table.[col1, row1], table.[col2, row2])
-
-let takeNext (table: char[,]) (text: StringBuilder) (pos: int) =
-    let mutable newPos = pos
+let populateTable (text: string) =
     let textLength = text.Length
-    while newPos < textLength && (not (existsInTable table (substitute (Char.ToLower text.[newPos])))) do
-        newPos <- newPos+1
+    let mutable place = 0
     
-    if (newPos >= textLength) then
-        ('x', newPos+1)
-    else 
-        (substitute (Char.ToLower text.[newPos]), (newPos+1))
+    for i in 0 .. textLength-1 do
+        let (col, row) = (place / dimension1, place % dimension1)
+        let character = text.[i]
+        let charPos = charValue character
+        if (positions.[charPos] = (0, 0) && table.[0, 0] <> character) then
+            table.[col, row] <- character
+            positions.[charPos] <- (col, row)
+            place <- place + 1
 
-let addEncryption (initialPos: int) ((char1, pos1): (char*int)) ((char2, pos2): (char*int)) table (text:StringBuilder) (encrypt:bool) = 
-    if encrypt then
-        let (encrypted1, encrypted2) = encryptPair char1 char2 table
-        // This is to keep the white-space and special caracters
-        // result <- result.Append (text.[initialPos .. (pos1-2)] + encrypted1.ToString())
-        // result <- result.Append (text.[pos1 .. (pos2-2)] + encrypted2.ToString())
-        (encrypted1.ToString() + encrypted2.ToString())
+let preprocessText (text: string) =
+    Regex.Replace(text.ToUpper().Replace("J", "I"), "[^A-Z]", "")
+
+let cipher (item1: (int*int)) (item2: (int*int)) (direction: int) =
+    let mutable (col1, row1) = item1
+    let mutable (col2, row2) = item2
+    if (row1 = row2) then
+        col1 <- (col1 + direction) % 5
+        col2 <- (col2 + direction) % 5
+    else if (col1 = col2) then
+        row1 <- (row1 + direction) % 5
+        row2 <- (row2 + direction) % 5
     else
-        let (decrypted1, decrypted2) = decryptPair char1 char2 table
-        // This is to keep the white-space and special caracters
-        // result <- result.Append (text.[initialPos .. (pos1-2)] + decrypted1.ToString())
-        // result <- result.Append (text.[pos1 .. (pos2-2)] + decrypted2.ToString())
-        (decrypted1.ToString() + decrypted2.ToString())
+        let tmp: int = col1
+        col1 <- col2
+        col2 <- tmp
 
-let iterateOnPairs (table: char[,]) (inputText: string) (encrypt: bool) =
-    let mutable i: int = 0
-    let mutable result = StringBuilder()
-    let mutable text: StringBuilder = StringBuilder inputText
+    let value1 = table.[col1, row1]
+    let value2 = table.[col2, row2]
+    value1.ToString() + value2.ToString()
+
+let iterateOnPairs (input: string) (direction: int) =
+    let mutable text: string = input
     
-    let mutable textLength = text.Length
-    while i <= textLength do
-        let (char1, pos1) = takeNext table text i
-        let (char2, pos2) = takeNext table text pos1
-        
-        if (pos1 >= textLength) then
-            i <- pos1
-        else 
-            if (char1 = char2 && char1 <> 'x' && encrypt) then
-                text <- text.Insert(pos1, "x")
-                textLength <- textLength+1
-            else
-                result <- result.Append (addEncryption i (char1, pos1) (char2, pos2) table text encrypt)
-                i <- pos2
+    let mutable result = StringBuilder()
+    let mutable i: int = 0
+    
+    while (i < text.Length)do
+        let first = positions.[(charValue text.[i])]
+        let second = positions.[(charValue text.[i + 1])]
+        result <- result.Append( (cipher first second direction) )
+        i <- i + 2
     result.ToString()
+
+let encrypt (text: string) =
+    let mutable sb: StringBuilder = StringBuilder(text)
+    let mutable i: int = 0
+    
+    while i < sb.Length do
+        if (i = sb.Length - 1) then
+            let mutable ending: string = ""
+            if (sb.Length % 2 = 1) then
+                ending <- "X"
+            sb <- sb.Append(ending)
+        else if (sb.[i] = sb.[i + 1]) then
+            sb <- sb.Insert((i + 1), "X")
+        i <- i + 2
+    iterateOnPairs (sb.ToString()) 1
+
+let decrypt (text: string) =
+    iterateOnPairs text 4
 
 [<EntryPoint>]
 let main argv =
-    let text = System.IO.File.ReadAllText("benchmarks/playfair_cipher/lines.txt")
-    let keyword: string = "thisisagreatkeyword"
+    let stop = System.Diagnostics.Stopwatch.StartNew()
+    let text = System.IO.File.ReadAllText("../lines.txt")
+    let keyword = "This is a great keyword"
+
+    populateTable (preprocessText (keyword + alphabet))
+    let processedText = preprocessText text
     
-    let mutable keyTable: char[,] = Array2D.create 5 5 Char.MaxValue
-    dimension1 <- keyTable.GetLength(0)
-    dimension2 <- keyTable.GetLength(1)
+    let encryption: string = encrypt processedText
+    let decryption: string = decrypt encryption
     
-    populateKeyTable keyTable keyword
-    
-    let encryption = iterateOnPairs keyTable text true
-    let decryption = iterateOnPairs keyTable encryption false
-    
-    System.IO.File.WriteAllText("../encrypted.txt", encryption)
-    System.IO.File.WriteAllText("../decrypted.txt", decryption)
-    
-    printfn "Length Encryption: %i" encryption.Length
-    printfn "Length Decryption: %i" decryption.Length
+    printfn "%i" encryption.Length
+    printfn "%i" decryption.Length
+    printfn "Time: %i" stop.ElapsedMilliseconds
     0 // return an integer exit code
