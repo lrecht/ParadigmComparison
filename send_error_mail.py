f8cd65d3fccd4290b947ae98a00e57e036d0748a
Martin Jensen <Martin.Jensen.1995@gmail.com>
2020-11-02 12:38:56 +0100

    Added send error python script, independent of runRAPL

---
+++ send_error_mail.py
@@ -0,0 +1,8 @@
+from email_service import send_fail
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-e", "--email", required=True, type=str, help="Send email")
+args = parser.parse_args()
+
+send_fail(args.email, "stdout.log", "stderr.log")
