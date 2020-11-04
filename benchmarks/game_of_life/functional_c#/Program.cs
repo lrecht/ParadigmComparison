722644aa3bc1ad170218c5ed06480330888692d5
Martin Jensen <Martin.Jensen.1995@gmail.com>
2020-11-04 19:56:17 +0100

    Shorter lines and less code

--- benchmarks/game_of_life/functional_c#/Program.cs
+++ benchmarks/game_of_life/functional_c#/Program.cs
@@ -11,10 +11,14 @@ namespace functional_c_
         static readonly int runs = 100;
         static void Main(string[] args)
         {
-            var initialStateRep = System.IO.File.ReadAllText("benchmarks/game_of_life/state256.txt").Select(x => x == '1');
+            var initialStateRep = System.IO.File.ReadAllText("benchmarks/game_of_life/state256.txt")
+                .Select(x => x == '1');
+            
             var indecies = Enumerable.Range(0, dimensions);
             var positions = indecies.SelectMany(x => indecies.Select(y => (x, y)));
-            var initialState = positions.Zip(initialStateRep, (k, v) => new KeyValuePair<(int x, int y), bool>(k, v)).ToImmutableDictionary<(int x, int y), bool>();
+            var initialState = positions
+                                .Zip(initialStateRep, (k, v) => new KeyValuePair<(int, int), bool>(k, v))
+                                .ToImmutableDictionary<(int x, int y), bool>();
             
             var result = simulateSteps(initialState, runs);
             
@@ -22,25 +26,23 @@ namespace functional_c_
             System.Console.WriteLine(living.Count());
         }
 
-        private static ImmutableDictionary<(int x, int y), bool> simulateSteps(ImmutableDictionary<(int x, int y), bool> state, int runs)
+        private static ImmutableDictionary<(int, int), bool> simulateSteps(ImmutableDictionary<(int, int), bool> state, int runs)
             => runs <= 0 ? state : simulateSteps(getNextState(state), runs - 1);
 
-        private static ImmutableDictionary<(int x, int y), bool> getNextState(ImmutableDictionary<(int x, int y), bool> state)
+        private static ImmutableDictionary<(int, int), bool> getNextState(ImmutableDictionary<(int x, int y), bool> state)
         {
             var relative = Enumerable.Range(-1, 3).ToImmutableList();
             return state.Select(point => {
                 var neighbourPositions = relative.SelectMany(x => relative.Select(y => (x, y)))
-                                        .Select(pos => (((((point.Key.x + pos.x) % dimensions) + dimensions) % dimensions) , ((((point.Key.y + pos.y) % dimensions) + dimensions) % dimensions)))
+                                        .Select(pos => (
+                                            ((((point.Key.x + pos.x) % dimensions) + dimensions) % dimensions), 
+                                            ((((point.Key.y + pos.y) % dimensions) + dimensions) % dimensions)))
                                         .Where(pos => point.Key != pos);
 
-                var liveNeighboursCount = neighbourPositions.Where(pos => state[pos]).Count();
+                var liveNeighbours = neighbourPositions.Where(pos => state[pos]).Count();
 
-                if(point.Value){
-                    return new KeyValuePair<(int x, int y), bool>(point.Key, liveNeighboursCount == 2 || liveNeighboursCount == 3);
-                }
-                else{
-                    return new KeyValuePair<(int x, int y), bool>(point.Key, liveNeighboursCount == 3);
-                }
+                var newPointValue = point.Value ? liveNeighbours == 2 || liveNeighbours == 3 : liveNeighbours == 3;
+                return new KeyValuePair<(int, int), bool>(point.Key, newPointValue);
             })
             .ToImmutableDictionary();
         }
