172e6a16589d649f1e801fc6e4a457391c5fff62
Martin Jensen <Martin.Jensen.1995@gmail.com>
2020-11-06 10:57:57 +0100

    Faster parallel c#

--- benchmarks/game_of_life_concurrent/functional_c#/Program.cs
+++ benchmarks/game_of_life_concurrent/functional_c#/Program.cs
@@ -8,48 +8,48 @@ namespace functional_c_
     class Program
     {
         static readonly int dimensions = 256;
+        static readonly int size = dimensions * dimensions;
         static readonly int runs = 100;
         static readonly int threads = System.Environment.ProcessorCount;
         static void Main(string[] args)
         {
             var initialStateRep = System.IO.File.ReadAllText("benchmarks/game_of_life/state256.txt")
-                .Select(x => x == '1');
+                .Select(x => x == '1')
+                .ToImmutableArray();
             
-            var indecies = Enumerable.Range(0, dimensions);
-            var positions = indecies.SelectMany(x => indecies.Select(y => (x, y)));
-            var initialState = positions
-                                .Zip(initialStateRep, (k, v) => new KeyValuePair<(int, int), bool>(k, v))
-                                .ToImmutableDictionary<(int x, int y), bool>();
+            var result = simulateSteps(initialStateRep, runs);
             
-            var result = simulateSteps(initialState, runs);
-            
-            var living = result.Where(x => x.Value);
+            var living = result.Where(x => x);
             System.Console.WriteLine(living.Count());
         }
 
-        private static ImmutableDictionary<(int, int), bool> simulateSteps(ImmutableDictionary<(int, int), bool> state, int runs)
+        private static ImmutableArray<bool> simulateSteps(ImmutableArray<bool> state, int runs)
             => runs <= 0 ? state : simulateSteps(getNextState(state), runs - 1);
 
-        private static ImmutableDictionary<(int, int), bool> getNextState(ImmutableDictionary<(int x, int y), bool> state)
+        private static ImmutableArray<bool> getNextState(ImmutableArray<bool> state)
         {
             var relative = Enumerable.Range(-1, 3).ToImmutableList();
+            var relativePositions = relative.SelectMany(x => relative.Select(y => (x, y)));
             return state.AsParallel()
                 .WithDegreeOfParallelism(threads)
                 .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
                 .WithMergeOptions(ParallelMergeOptions.FullyBuffered)
-                .Select(point => {
-                var neighbourPositions = relative.SelectMany(x => relative.Select(y => (x, y)))
+                .Select((x, i) => {
+                    var iCoordinates = ((i % dimensions), (i / dimensions));
+
+                    var neighbourCoordinates = relativePositions
                                         .Select(pos => (
-                                            ((((point.Key.x + pos.x) % dimensions) + dimensions) % dimensions), 
-                                            ((((point.Key.y + pos.y) % dimensions) + dimensions) % dimensions)))
-                                        .Where(pos => point.Key != pos);
+                                            ((((i % dimensions + pos.x) % dimensions) + dimensions) % dimensions), 
+                                            ((((i / dimensions + pos.y) % dimensions) + dimensions) % dimensions)))
+                                        .Where(x => x != iCoordinates);
 
-                var liveNeighbours = neighbourPositions.Where(pos => state[pos]).Count();
+                var neighbourIndecies = neighbourCoordinates.Select(x => (dimensions * x.Item2 + x.Item1));
 
-                var newPointValue = point.Value ? liveNeighbours == 2 || liveNeighbours == 3 : liveNeighbours == 3;
-                return new KeyValuePair<(int, int), bool>(point.Key, newPointValue);
+                var liveNeighbours = neighbourIndecies.Where(j => state[j]).Count();
+
+                return x ? liveNeighbours == 2 || liveNeighbours == 3 : liveNeighbours == 3;
             })
-            .ToImmutableDictionary();
+            .ToImmutableArray();
         }
     }
 }
