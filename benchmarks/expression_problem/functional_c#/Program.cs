50559fddebb8287310b0847a5bcafd6c6bc61e60
Martin Jensen <Martin.Jensen.1995@gmail.com>
2020-11-09 12:12:24 +0100

    More functional functional

--- benchmarks/expression_problem/functional_c#/Program.cs
+++ benchmarks/expression_problem/functional_c#/Program.cs
@@ -19,46 +19,43 @@ namespace functional_c_
             if(runs <= 0)
                 return (printCount, evalCount);
 
-            var max = 1000;
-            var count = 0;
-            var (exprPrintFn, exprEvalFn) = generateRandomExpression(max, ref count, rand);
+            var (exprPrintFn, exprEvalFn) = generateRandomExpression(1000, rand);
             return runHelper(runs - 1, printCount + exprPrintFn().Length, evalCount + exprEvalFn(), rand);
         }
 
-        private static (Func<string> print, Func<int> eval) generateRandomExpression(int max, ref int count, Random rand)
+        private static (Func<string> print, Func<int> eval) generateRandomExpression(int count, Random rand)
         {
-            if(count >= max){
+            if(count <= 0){
                 var lit = rand.Next(0, 100);
                 return (() => lit.ToString(), () => lit);
             }
 
-            count++;
             var x = rand.Next(0, 4);
 
             switch(x){
                 case 0: {
-                    var (printFn1, evalFn1) = generateRandomExpression(max, ref count, rand);
-                    var (printFn2, evalFn2) = generateRandomExpression(max, ref count, rand);
+                    var (printFn1, evalFn1) = generateRandomExpression(count - 1, rand);
+                    var (printFn2, evalFn2) = generateRandomExpression(0, rand);
                     Func<string> newPrintFn = () => "(" + printFn1() + "+" + printFn2() + ")";
                     Func<int> newEvalFn = () => evalFn1() + evalFn2();
                     return (newPrintFn, newEvalFn);
                 };
                 case 1: {
-                    var (printFn1, evalFn1) = generateRandomExpression(max, ref count, rand);
-                    var (printFn2, evalFn2) = generateRandomExpression(max, ref count, rand);
+                    var (printFn1, evalFn1) = generateRandomExpression(count - 1, rand);
+                    var (printFn2, evalFn2) = generateRandomExpression(0, rand);
                     Func<string> newPrintFn = () => "(" + printFn1() + "-" + printFn2() + ")";
                     Func<int> newEvalFn = () => evalFn1() - evalFn2();
                     return (newPrintFn, newEvalFn);
                 }
                 case 2:{
-                    var (printFn1, evalFn1) = generateRandomExpression(max, ref count, rand);
-                    var (printFn2, evalFn2) = generateRandomExpression(max, ref count, rand);
+                    var (printFn1, evalFn1) = generateRandomExpression(count - 1, rand);
+                    var (printFn2, evalFn2) = generateRandomExpression(0, rand);
                     Func<string> newPrintFn = () => "(" + printFn1() + "*" + printFn2() + ")";
                     Func<int> newEvalFn = () => evalFn1() * evalFn2();
                     return (newPrintFn, newEvalFn);
                 }
                 case 3: {
-                    var (printFn, evalFn) = generateRandomExpression(max, ref count, rand);
+                    var (printFn, evalFn) = generateRandomExpression(count - 1, rand);
                     Func<string> newPrintFn = () => "(-" + printFn() + ")";
                     Func<int> newEvalFn = () => -evalFn();
                     return (newPrintFn, newEvalFn);
