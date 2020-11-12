23b298c51712aa5a8112b968a30f7095fae05b67
Martin Jensen <Martin.Jensen.1995@gmail.com>
2020-11-12 09:58:01 +0100

    Much faster functional c#... but still relatively slow

--- benchmarks/kmeans/functional_c#/Program.cs
+++ benchmarks/kmeans/functional_c#/Program.cs
@@ -5,13 +5,6 @@ using System.Collections.Generic;
 
 namespace functional_c_
 {
-    readonly struct Cluster {
-        public Cluster((double x, double y) mean){
-            this.mean = mean;
-        }
-        public readonly (double x, double y) mean;
-    }
-
     class Program
     {
         public static (double x, double y) computMean(IEnumerable<(double x, double y)> points){
@@ -22,35 +15,35 @@ namespace functional_c_
             return (sumX/pointsSize, sumY/pointsSize);
         }
 
-        public static Cluster closest((double x, double y) point, ImmutableList<Cluster> clusters)
+        public static (double, double) closest((double x, double y) point, ImmutableList<(double x, double y)> clusters)
             => clusters.OrderBy(c => euclideanDist(point, c)).First();
 
-        private static double euclideanDist((double x, double y) point, Cluster c)
-            => Math.Sqrt(Math.Pow(point.x - c.mean.x, 2) + Math.Pow(point.y - c.mean.y, 2));
+        private static double euclideanDist((double x, double y) point, (double x, double y) c)
+            => Math.Sqrt(Math.Pow(point.x - c.x, 2) + Math.Pow(point.y - c.y, 2));
 
         static void Main(string[] args)
         {
             var points = System.IO.File.ReadAllLines("benchmarks/kmeans/points.txt").Select(x => (double.Parse(x.Split(':')[0]), double.Parse(x.Split(':')[1]))).ToImmutableList();
-            var clusters = runKMeans(2, points);
-            clusters.ForEach(c => System.Console.WriteLine(c.mean));
+            var clusters = runKMeans(10, points);
+            clusters.ForEach(c => System.Console.WriteLine(c));
         }
 
-        private static ImmutableList<Cluster> runKMeans(int clusterCount, ImmutableList<(double x, double y)> points)
+        private static ImmutableList<(double x, double y)> runKMeans(int clusterCount, ImmutableList<(double x, double y)> points)
         {
-            var clusters = points.Take(clusterCount).Select(p => new Cluster(p)).ToImmutableList();
+            var clusters = points.Take(clusterCount).ToImmutableList();
             return converge(clusters, points);
         }
 
-        private static ImmutableList<Cluster> converge(ImmutableList<Cluster> clusters, ImmutableList<(double x, double y)> points)
+        private static ImmutableList<(double, double)> converge(ImmutableList<(double x, double y)> clusters, ImmutableList<(double x, double y)> points)
         {
             var groups = points
                         .GroupBy(p => closest(p, clusters));
 
             var newClusters = groups
-                        .Select(g => new Cluster(computMean(g)))
+                        .Select(g => computMean(g))
                         .ToImmutableList();
 
-            if(clusters.All(c => newClusters.Any(nc => nc.mean == c.mean)))
+            if(clusters.SequenceEqual(newClusters))
                 return clusters;
             else
                 return converge(newClusters, points);
