28f1336f3f2ba12788bf8de7de2dfd75eea77656
Martin Jensen <Martin.Jensen.1995@gmail.com>
2020-11-13 14:52:49 +0100

    Functional c# spanning tree

---
+++ benchmarks/spanning_tree/functional_c#/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Immutable;
+
+namespace functional_c_
+{
+    class Program
+    {
+        static readonly int magicVertexCount = 5877;
+        static void Main(string[] args)
+        {
+            var edgesRep = System.IO.File.ReadAllLines("benchmarks/spanning_tree/graph.csv")
+                            .Select(l => l.Split(','))
+                            .Select((val, i) => (i, Convert.ToInt32(val[0]), Convert.ToInt32(val[1]), Convert.ToInt32(val[2])));
+
+            var edges = edgesRep.OrderBy(e => e.Item4).ToImmutableList();
+            var edgesDict = edgesRep.ToImmutableDictionary(e => e.i);
+
+            var spanningTreeEdges = getMinimumSpanningTree(edges);
+            var resultEdges = spanningTreeEdges.Select(i => edgesDict[i]);
+
+            var totalweight = resultEdges.Sum(e => e.Item4);
+            var totalEdges = resultEdges.Count();
+            
+            System.Console.WriteLine(totalEdges);
+            System.Console.WriteLine(totalweight);
+        }
+
+        static ImmutableList<int> getMinimumSpanningTree(ImmutableList<(int, int, int, int)> edges)
+            => getMinimumSpanningTreeHelper(edges, 0, ImmutableList<int>.Empty, ImmutableDictionary<int, int>.Empty);
+
+        static ImmutableList<int> getMinimumSpanningTreeHelper(ImmutableList<(int i, int v1, int v2, int)> edges, int i, ImmutableList<int> spanningTree, ImmutableDictionary<int, int> rootDict){
+            if(spanningTree.Count >= magicVertexCount - 1) //-1 as one less edge than vertex is required to complete graph
+                return spanningTree;
+
+            var currentEdge = edges[i];
+            var (res, updatedDict) = union(currentEdge.v1, currentEdge.v2, rootDict);
+
+            if(res)
+                return getMinimumSpanningTreeHelper(edges, i + 1, spanningTree.Add(currentEdge.i), updatedDict);
+            else
+                return getMinimumSpanningTreeHelper(edges, i + 1, spanningTree, updatedDict);
+        }
+
+        private static (bool, ImmutableDictionary<int, int>) union(int v1, int v2, ImmutableDictionary<int, int> rootDict)
+        {
+            var group1Root = find(v1, rootDict);
+            var group2Root = find(v2, rootDict);
+
+            var updatedDict = rootDict.SetItem(v1, group1Root).SetItem(v2, group2Root);
+
+            if(group1Root == group2Root)
+                return (false, updatedDict);
+            else
+                return (true, updatedDict.SetItem(group2Root, group1Root));
+        }
+
+        private static int find(int vertex, ImmutableDictionary<int, int> rootDict)
+            => rootDict.ContainsKey(vertex) && rootDict[vertex] != vertex ? find(rootDict[vertex], rootDict) : vertex;
+    }
+}
