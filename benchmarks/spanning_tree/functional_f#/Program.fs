ba656cec562b74cfa424c70f4e1193bc51b6eac7
Lars <lrecht16@student.aau.dk>
2020-11-16 15:47:52 +0100

    Fixed bug and quick fix

--- benchmarks/spanning_tree/functional_f#/Program.fs
+++ benchmarks/spanning_tree/functional_f#/Program.fs
@@ -2,37 +2,37 @@
 open System.Collections.Generic
 
 let compare (from,dest,w) (from1,dest1,w1) =
-    if w < w1 then 1
-    elif w > w1 then -1
-    elif from < from1 then 1
-    elif from > from1 then -1
-    elif dest < dest1 then 1
-    elif dest > dest1 then -1
+    if w < w1 then -1
+    elif w > w1 then 1
+    elif from < from1 then -1
+    elif from > from1 then 1
+    elif dest < dest1 then -1
+    elif dest > dest1 then 1
     else 0
 
-let rec find node (uf:ImmutableArray<int32>) changes =
-    if uf.[node] < 0 || List.contains node changes
-    then (node,(List.fold (fun (uf:ImmutableArray<int32>) change -> uf.Insert(node,change)) uf changes))
+let rec find node (uf:ImmutableArray<int32>) =
+    if uf.[node] < 0
+    then node,uf
     else 
-        find uf.[node] uf (node::changes)
+        let node1,uf1 = find uf.[node] uf
+        node1,uf1.SetItem(node,node1)
 
 let rec spanningTree' (edges:ImmutableSortedSet<int*int*int>) (uf:ImmutableArray<int32>) weightSum edgeNumber vCount =
     if vCount = 0 then (weightSum,edgeNumber)
-    elif edges.IsEmpty then (-1,-1)
     else 
         let (f,d,w) = edges.Min
-        let root1,uf1 = find f uf []
-        let root2,uf2 = find d uf1 []
-        if root1 = root2 then spanningTree' (edges.Remove (f,d,w)) uf2 weightSum edgeNumber vCount
-        else spanningTree' (edges.Remove (f,d,w)) (uf2.Insert(root1,root2)) (weightSum+w) (edgeNumber+1) (vCount-1)
+        let root1,uf1 = find f uf
+        let root2,uf2 = find d uf1
+        if root1 = root2 then spanningTree' (edges.Remove (f,d,w)) uf weightSum edgeNumber vCount
+        else spanningTree' (edges.Remove (f,d,w)) (uf2.SetItem(root1,root2)) (weightSum+w) (edgeNumber+1) (vCount-1)
 
 let spanningTree (edges:ImmutableSortedSet<int*int*int>) =
-    spanningTree' edges ([|for i in 0 .. 6006 do -1|].ToImmutableArray()) 0 0 6004
+    spanningTree' edges ([|for i in 0 .. 6006 do -1|].ToImmutableArray()) 0 0 5876
 
 [<EntryPoint>]
 let main argv =
     let iss = ImmutableSortedSet.Empty.WithComparer (Comparer<int*int*int>.Create (fun x1 x2 -> compare x1 x2))
-    let res = System.IO.File.ReadAllLines("benchmarks/spanning_tree/graph.csv")
+    let res = System.IO.File.ReadAllLines("/home/lars/Documents/9. Semester/ParadigmComparison/benchmarks/spanning_tree/graph.csv")
     let full = Array.fold (fun (acc:ImmutableSortedSet<int*int*int>) (s:string) -> 
         let sarr = s.Split ',' in acc.Add((int32 sarr.[0]), (int32 sarr.[1]), (int32 sarr.[2]))) iss res
     let weight,edge = spanningTree full
