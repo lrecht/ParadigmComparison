52f6ffa47cadae90039edd4578202142461c5a78
Martin Jensen <Martin.Jensen.1995@gmail.com>
2020-12-11 20:21:32 +0100

    Performance improvement: playfair cipher functional c# *Lists -> Arrays (Except when adding a lot) *More use of tuples when known lenght *On my maching: ~3220ms -> ~1715ms

--- benchmarks/playfair_cipher/functional_c#/Program.cs
+++ benchmarks/playfair_cipher/functional_c#/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Immutable;
+using System.Collections.Generic;
 
 namespace functional_c_
 {
@@ -14,92 +15,93 @@ namespace functional_c_
 
             var TEST_STRING = System.IO.File.ReadAllText("benchmarks/playfair_cipher/lines.txt");
             var table = createTable(keyword);
-            var encoded = new string(encode(TEST_STRING, table).ToArray());
-            var decoded = new string(decode(encoded, table).ToArray());
+            var encoded = encode(TEST_STRING, table);
+            var decoded = decode(encoded, table);
 
             System.Console.WriteLine(encoded.Length);
             System.Console.WriteLine(decoded.Length);
         }
 
         private static (ImmutableDictionary<char, (int, int)>, ImmutableDictionary<(int, int), char>) createTable(string keyword)
-            => createTableHelper(prepKey(keyword), 1, 1, (ImmutableDictionary<char, (int, int)>.Empty, ImmutableDictionary<(int, int), char>.Empty));
+            => createTableHelper(prepKey(keyword), 0, 1, 1, (ImmutableDictionary<char, (int, int)>.Empty, ImmutableDictionary<(int, int), char>.Empty));
 
         private static (ImmutableDictionary<char, (int, int)>, ImmutableDictionary<(int, int), char>) createTableHelper(
-            ImmutableList<char> keyword, 
+            ImmutableArray<char> keyword, 
+            int keywordIndex,
             int rowIndex, 
             int colIndex, 
             (ImmutableDictionary<char, (int, int)>, ImmutableDictionary<(int, int), char>) table)
         {
-            if(keyword.IsEmpty)
+            if(keywordIndex >= keyword.Length)
                 return table;
             
             var (postions, values) = table;
-            var c = keyword.First();
+            var c = keyword[keywordIndex];
             var updatedPoss = postions.Add(c, (rowIndex, colIndex));
             var updatedVals = values.Add((rowIndex, colIndex), c);
-            return createTableHelper(keyword.RemoveAt(0), 
+            return createTableHelper(keyword, 
+                                    keywordIndex + 1,
                                     ((rowIndex % 5) + 1),
                                     (colIndex + rowIndex / 5),
                                     (updatedPoss, updatedVals));
         }
 
-        private static ImmutableList<char> prepKey(string keyword)
+        private static ImmutableArray<char> prepKey(string keyword)
         {
             var simpleKey = keyword
                             .Where(x => char.IsLetter(x))
                             .Distinct()
                             .Select(x => char.ToUpper( x == 'j' || x == 'J' ? 'I' : x ))
-                            .ToImmutableList();
+                            .ToImmutableArray();
 
             var smallerAlph = alphabet
                             .Where(x => !simpleKey.Contains(x))
-                            .ToImmutableList();
+                            .ToImmutableArray();
 
             return simpleKey.AddRange(smallerAlph);
         }
 
-        private static ImmutableList<char> prepInput(ImmutableList<char> str)
+        private static ImmutableArray<char> prepInput(string str)
             => str
                 .Select(c => char.ToUpper( c == 'j' || c == 'J' ? 'I' : c ))
                 .Where(c => alphabet.Contains(c))
-                .ToImmutableList();
+                .ToImmutableArray();
 
-        private static ImmutableList<char> encode(string str, (ImmutableDictionary<char, (int, int)>, ImmutableDictionary<(int, int), char>) table)
+        private static string encode(string str, (ImmutableDictionary<char, (int, int)>, ImmutableDictionary<(int, int), char>) table)
             => cipher(encodePair, str, table);
 
-        private static ImmutableList<char> decode(string str, (ImmutableDictionary<char, (int, int)>, ImmutableDictionary<(int, int), char>) table)
+        private static string decode(string str, (ImmutableDictionary<char, (int, int)>, ImmutableDictionary<(int, int), char>) table)
             => cipher(decodePair, str, table);
 
-        private static ImmutableList<char> cipher(
-            Func<char, char, (ImmutableDictionary<char, (int, int)>, ImmutableDictionary<(int, int), char>), ImmutableList<char>> func, 
+        private static string cipher(
+            Func<char, char, (ImmutableDictionary<char, (int, int)>, ImmutableDictionary<(int, int), char>), (char c1, char c2)> func, 
             string str, 
             (ImmutableDictionary<char, (int, int)>, ImmutableDictionary<(int, int), char>) table)
         {
-            var prep = prepInput(str.ToImmutableList());
+            var prep = prepInput(str);
 
             var insertedRare = prep.Aggregate(ImmutableList<char>.Empty, ((acc, c) => acc.Count % 2 == 1 && acc.Last() == c ? acc.Add(rare).Add(c) : acc.Add(c)));
-            var evenLengthString = insertedRare.Count % 2 == 0 ? insertedRare : insertedRare.Add(rare);
+            var evenLengthString = (insertedRare.Count % 2 == 0 ? insertedRare : insertedRare.Add(rare)).ToImmutableArray();
 
-            return evenLengthString
-                .Where((c, i) => i % 2 == 0)
-                .Select((c, i) => (c, evenLengthString[i * 2 + 1]))
-                .Select(charPair => func(charPair.c, charPair.Item2, table))
-                .Aggregate((str1, str2) => str1.AddRange(str2));
+            var charPairs = Enumerable.Range(0, evenLengthString.Length / 2)
+                .Select(i => func(evenLengthString[i * 2], evenLengthString[i * 2 + 1], table));
+
+            return string.Join("", charPairs.Select(t => t.c1.ToString() + t.c2.ToString()));
         }
 
-        private static ImmutableList<char> encodePair(
+        private static (char, char) encodePair(
             char first, 
             char second, 
             (ImmutableDictionary<char, (int, int)>, ImmutableDictionary<(int, int), char>) table)
                 => pairHelper((x => x % 5 + 1), first, second, table);
 
-        private static ImmutableList<char> decodePair(
+        private static (char, char) decodePair(
             char first, 
             char second, 
             (ImmutableDictionary<char, (int, int)>, ImmutableDictionary<(int, int), char>) table)
                 => pairHelper((x => x == 1 ? 5 : x - 1), first, second, table);
 
-        private static ImmutableList<char> pairHelper(
+        private static (char, char) pairHelper(
             Func<int, int> adjust, 
             char first, 
             char second, 
@@ -113,17 +115,17 @@ namespace functional_c_
             else if(row1 == row2){
                 var c1 = findVal(adjust(col1), row1, table);
                 var c2 = findVal(adjust(col2), row2, table);
-                return ImmutableList<char>.Empty.Add(c1).Add(c2);
+                return (c1, c2);
             }
             else if(col1 == col2){
                 var c1 = findVal(col1, adjust(row1), table);
                 var c2 = findVal(col2, adjust(row2), table);
-                return ImmutableList<char>.Empty.Add(c1).Add(c2);
+                return (c1, c2);
             }
             else{
                 var c1 = findVal(col2, row1, table);
                 var c2 = findVal(col1, row2, table);
-                return ImmutableList<char>.Empty.Add(c1).Add(c2);
+                return (c1, c2);
             }
         }
 
