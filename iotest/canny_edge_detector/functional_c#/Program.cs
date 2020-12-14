790a5e16c025a8893aff5207dc1e6ac45559fab6
Anne Ejsing <anneejsing77@gmail.com>
2020-12-14 09:29:08 +0100

    Create iotests for canny

---
+++ iotest/canny_edge_detector/functional_c#/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Collections.Immutable;
+
+namespace functional_c_
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var pic = new Bitmap("iotest/canny_edge_detector/download.jpg");
+            System.Console.WriteLine(pic.Width);
+        }
+    }
+}
---
+++ iotest/canny_edge_detector/functional_f#/Program.fs
@@ -0,0 +1,8 @@
+﻿open System
+open System.Drawing
+
+[<EntryPoint>]
+let main argv =
+    let image = new Bitmap("iotest/canny_edge_detector/download.jpg")
+    printfn "%i" image.Width
+    0 // return an integer exit code
