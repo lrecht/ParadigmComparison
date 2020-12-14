efe19c6edc956d22b6e72b066508ade8f44594a9
Anne Ejsing <anneejsing77@gmail.com>
2020-12-14 09:44:16 +0100

    Create iotest for Hough transform

---
+++ iotest/hough_transform/functional_c#/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Collections.Immutable;
+
+namespace functional_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pic = new Bitmap("iotest/hough_transform/Pentagon.png");
+            System.Console.WriteLine(pic.Width);
+        }
+    }
+}
---
+++ iotest/hough_transform/functional_f#/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+open System.Drawing
+
+let bm = new Bitmap("iotest/hough_transform/Pentagon.png")
+
+[<EntryPoint>]
+let main argv =
+    let c = bm.Width
+    printfn "%i" c
+    0 // return an integer exit code
