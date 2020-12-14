d4f3243cbfa997c36c8ad661c282b0696b8e67a9
lrecht <lrecht16@student.aau.dk>
2020-12-14 11:03:55 +0100

    Adds the option of making a table with the EDP's of each implementation.

---
+++ processing/EDPPlotter.py
@@ -0,0 +1,69 @@
+import itertools
+import os
+from typing import *
+from Result import Key, MetricType, Result
+
+class EDPPrinter():
+    def __init__(self, weight: int,outputfile):
+        self.weight = weight
+        self.outputfile = outputfile
+
+    def computeEDP(self,results):
+        edp = {}
+        for k in results.observations:
+            # Only need one result pr. benchmark, language and paradigm
+            if k.metric is not MetricType.DURATION:
+                continue
+
+            # Find results for time, dram and package
+            time = results.observations[k]
+            k = Key(benchmark=k.benchmark,paradigm=k.paradigm,language=k.language,metric=MetricType.PACKAGE)
+            cpu = results.observations[k]
+            k = Key(benchmark=k.benchmark,paradigm=k.paradigm,language=k.language,metric=MetricType.DRAM)
+            ram = results.observations[k]
+
+            length = len(time)
+            sumTime = 0
+            sumCPU = 0
+            sumRAM = 0
+
+            # Summarise the results
+            for i in range(0,length):
+                sumTime = sumTime + time[i]
+                sumCPU = sumCPU + cpu[i]
+                sumRAM = sumRAM + ram[i]
+
+            # Find avg's and convert to seconds and joule
+            avgTime = sumTime / length * 0.000001
+            avgCPU = sumCPU / length * 0.000001
+            avgRAM = sumRAM / length * 0.000001
+
+            edp[k] = avgTime ** self.weight * (avgCPU+avgRAM)
+        
+        return edp
+
+    # Creates output as: |       c#      |      f#       |
+    #                    | pp | oop | fp | pp | oop | fp |
+    # Output fits under this table header
+    def createOutput(self,edp,results):
+        lines = []
+        for benchmark in results.benchmarks:
+            line = benchmark.replace('_', ' ').capitalize()
+            for lang in results.languages:
+                for par in ['procedural','oop','functional']:
+                    line = line + " &"
+                    if par in results.paradigms[benchmark]:
+                        res = edp[Key(benchmark,par,lang,MetricType.DRAM)]
+                        line = line + " %s" % float('%.5g' % res)
+            line = line + " \\\\ \hline \n"
+            lines.append(line)
+        return lines
+
+    def save(self, lines):
+        with open(self.outputfile, 'w+') as output:
+            output.writelines(lines)
+
+    def output(self, results):
+        edp = self.computeEDP(results)
+        out = self.createOutput(edp,results)
+        self.save(out)
