e7596963007ba7d8ee865cad4c8d2812a036e694
Anne Ejsing <anneejsing77@gmail.com>
2020-12-14 09:48:46 +0100

    Create iotest for huffman coding

---
+++ iotest/huffman_coding/functional_c#/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Collections.Immutable;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace functional_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var TEST_STRING = File.ReadAllText("iotest/huffman_coding/lines.txt");
+            System.Console.WriteLine(TEST_STRING.Length);
+        }
+    }
+}
---
+++ iotest/huffman_coding/functional_f#/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+open System.Collections.Immutable
+open System.Collections.Generic
+
+let filePath = "iotest/huffman_coding/lines.txt"
+let input = File.ReadAllText filePath
+
+
+[<EntryPoint>]
+let main argv =
+    let c = input.Length
+    printfn "%i" c
+    0 // return an integer exit code
