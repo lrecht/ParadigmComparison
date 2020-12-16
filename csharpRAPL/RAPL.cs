d44a3250e402e2cabb92426fd87bfa78f3605712
Martin Jensen <Martin.Jensen.1995@gmail.com>
2020-12-16 15:32:15 +0100

    csharpRAPL
    *Implemented rapl library in line with pyRAPL
    *benchmarking library uses this and creates csv file with results

---
+++ csharpRAPL/RAPL.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace csharpRAPL
+{
+    public class RAPL
+    {
+        private List<Sensor> apis = new List<Sensor>{
+            new Sensor("pkg", new PackageAPI(), CollectionApproach.DIFFERENCE), 
+            new Sensor("dram", new DramAPI(), CollectionApproach.DIFFERENCE),
+            new Sensor("temp", new TempAPI(), CollectionApproach.AVERAGE)
+        };
+
+        private Stopwatch sw = new Stopwatch();
+        private TimeSpan elapsedTime;
+        
+        public void Start() 
+        {
+            sw.Reset();
+            apis.ForEach(api => api.Start());
+            sw.Start();
+        }
+
+        public void End()
+        {
+            sw.Stop();
+            apis.ForEach(api => api.End());
+            elapsedTime = sw.Elapsed;
+        }
+
+        //Not general. Only returns one result as our pcs have one socket
+        public (TimeSpan, List<(string deviceName, double energyUsed)>) getResult()
+        {
+            var results = apis.Select(api => (api.Name, api.Delta()[0])).ToList();
+            return (elapsedTime, results);
+        }
+    }
+}
--- library_benchmark/benchmark.cs
+++ library_benchmark/benchmark.cs
@@ -1,59 +1,91 @@
 ﻿using System;
+using System.IO;
 using System.Diagnostics;
+using System.Collections.Generic;
+using csharpRAPL;
+
+struct Measure
+{
+    public TimeSpan duration;
+    public List<(string apiName, double apiValue)> apis;
+    
+    public Measure((TimeSpan, List<(string, double)>) raplResult){
+        this.duration = raplResult.Item1;
+        this.apis = raplResult.Item2;
+    }
+}
 
 namespace benchmark
 {
     public class Benchmark
     {
-        Stopwatch sw = new Stopwatch();
-        float elapsedTime;
         static readonly string outputFilePath = "tempResults.csv";
         int iterations { get; }
+        List<Measure> _resultBuffer = new List<Measure>();
+        RAPL _rapl;
 
         public Benchmark(int iterations) 
         {
             this.iterations = iterations;
+            this._rapl = new RAPL();
         }
 
         public void Start() 
         {
-            sw.Reset();
-            sw.Start();
+            _rapl.Start();
         }
 
         public void End()
         {
-            sw.Stop();
-            elapsedTime = sw.ElapsedMilliseconds;
+            _rapl.End();
+            _resultBuffer.Add(new Measure(_rapl.getResult()));
         }
 
         public void Run<I, R>(Func<I, R> benchmark, I input, Action<R> print) 
         {
+            _resultBuffer = new List<Measure>();
             for (int i = 0; i < iterations; i++)
             {
                 Start();
                 R res = benchmark(input);
                 End();
-                SaveResults();
                 print(res);
             }
+            SaveResults();
         }
 
         public void Run<R>(Func<R> benchmark, Action<R> print) 
         {
+            _resultBuffer = new List<Measure>();
             for (int i = 0; i < iterations; i++)
             {
                 Start();
                 R res = benchmark();
                 End();
-                SaveResults();
                 print(res);
             }
+            SaveResults();
         }
 
         public void SaveResults()
         {
-            System.IO.File.AppendAllText(outputFilePath, elapsedTime + "\n");
+            var header = "duration(ms);pkg(µj);dram(µj);temp(C)" + "\n";
+            string result = header;
+            
+            foreach (Measure m in _resultBuffer)
+            {
+                result += m.duration.TotalMilliseconds;
+                foreach (var res in m.apis)
+                {
+                    if (res.apiName.Equals("temp"))
+                        result += ";" + ((double)res.apiValue / 1000);
+                    else
+                        result += ";" + res.apiValue;
+                }
+                result += "\n";
+            }
+
+            File.WriteAllText(outputFilePath, result);
         }
     }
 }
